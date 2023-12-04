93fbe627e71b0bdc2712eac6111324ea6b10a4c8
zinnurov <zinnurov@yandex-team.ru>
2023-12-04 22:32:42 +0300

    added 2 types of req

--- server/server.js
+++ server/server.js
@@ -1,19 +1,46 @@
 const express = require('express')
-
+const jwt = require('jsonwebtoken')
 
 
 const SERVER_PORT = 5000
 
 
 const app = express()
+app.use(express.json())
 
 const start = async () =>{
     try {
-        app.listen(SERVER_PORT, ()=>{
+
+        app.get('/', (req,res)=>{
+            res.send("Server on work")
+
+        })
+
+
+        
+        app.post('/user/login', (req, res)=>{
+            console.log("ERROR")
+            console.log(req.body)
+            const usertoken = jwt.sign({
+                password: req.body.password,
+            }, '3')
+            console.log("ERROR")
+            res.json({
+                status: true,
+                usertoken,
+
+            })
+        })
+
+
+        app.listen(SERVER_PORT, (err)=>{
+            if(err){
+                return console.log('server error' + err)
+            }
             console.log(`Server started on PORT: ${SERVER_PORT}`)
         })
-    } catch (error) {
-        console.log(error)
+    } catch (err) {
+        console.log(err)
         
     }
 }
